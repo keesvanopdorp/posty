9900b0350b8cbe8b3ebf63ab0c6ae06eec73d366
kees <keesjrvanopdorp@live.nl>
2021-02-11 07:44:24 +0100

    changes post_likes.php tp postLikes

--- app/Models/PostLikes.php
+++ app/Models/PostLikes.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class post_likes extends Model
+class PostLikes extends Model
 {
     use HasFactory;
+    protected $table = "post_likes";
 }
