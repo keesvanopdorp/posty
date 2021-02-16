8b2c09fe57aaa7aa535f97736b87026d2452c43e
Kees van Opdorp <keesjrvanopdorp@live.nl>
2021-02-16 16:16:54 +0100

    added message after email verification

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,7 @@ Route::get('/email/verify', function () {
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
-
-    return redirect('/');
+    return redirect('/')->with("success", "Email Verified");
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::post('/email/verification-notification', function (Request $request) {
